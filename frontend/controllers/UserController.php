e26bedeee10b3d40192e147ebf88ee9da366c346
Damien Buttler <damien@doublehops.com>
2016-03-14 18:55:18 +1100

    Added UserSearch method for refined search.

--- frontend/controllers/UserController.php
+++ frontend/controllers/UserController.php
@@ -2,10 +2,25 @@
 
 namespace frontend\controllers;
 
+use yii;
 use yii\rest\ActiveController;
 
 class UserController extends ActiveController
 {
     public $modelClass = 'common\models\User';
 
+
+    public function actions()
+    {
+        $actions = parent::actions();
+        $actions['index']['prepareDataProvider'] = [$this, 'prepareDataProvider'];
+
+        return $actions;
+    }
+
+    public function prepareDataProvider()
+    {
+        $searchModel = new \common\models\UserSearch();
+        return $searchModel->search(Yii::$app->request->queryParams);
+    }
 }
